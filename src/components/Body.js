52d39379190ad04b87b67f79633354aedc9cc673
ramjayakumar21 <ramjayakumar21@gmail.com>
2022-08-25 09:17:48 -0700

    Add drag and drop basic functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,29 +1,53 @@
 import React from "react";
+import "./Body.css"
+import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
+
+let tasks = [
+    {
+        id: '1',
+        name: 'task 1',
+        priority: 'high',
+    },
+    {
+        id: '2',
+        name: 'task 2',
+        priority: 'low',
+    }
+];
+
+
 
 export default function Body() {
+    let tasksList = tasks.map(({...task}, index) => {
+        return ( 
+        <Draggable key={task.id} draggableId={task.id} index={index}>
+            {(provided) => 
+                <li className="task" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                    <h1>{task.name}</h1>
+                    <p>{task.priority}</p>
+                </li>}
+        </Draggable>
+        )
+    })
+
+
     return (
         <div>
             <h1>Current tasks</h1>
             <input id='addtasks'></input>
             <button id='btn'>Add task</button>
-            <div id="array"></div>
+            <DragDropContext>
+                <Droppable droppableId="task-list" >
+                    {(provided) => 
+                        <ul className="task-list" ref={provided.innerRef} {...provided.droppableProps} >
+                            {tasksList}
+                        </ul>
+                    }
+                </Droppable>
+            </DragDropContext>
         </div>
     )
 }
 
-let tasks = ['test'];
-
-let b = document.getElementById('btn');
-
-b.addEventListener('click', () => {
-    displayArray();
-})
 
 
-function displayArray() {
-    tasks.forEach(task => {
-        let t = document.createElement('h1');
-        t.textContent = task;
-        document.getElementsById('array').appendChild(t);
-    })
-}
